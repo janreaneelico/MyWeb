e9f4d34afbeb66345b37b170e9ca2d60c022ab46
janreaneelico <153175210+janreaneelico@users.noreply.github.com>
2023-12-07 17:13:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,6 @@ st.header("About me:")
 st.write("##")
 st.write(
     """
-    - Actually I love programming when I was in my senior high I always have this dreams/goals that I want to be a programmer someday.
-    - I have a fervent desire to embark on a journey of continuous learning, particularly in the realm of programming and coding. The allure of these disciplines captivates me, and I am resolute in my commitment to unravel their complexities. 
-    - As I set forth on this educational odyssey, my primary objective is to cultivate a profound understanding of programming languages and coding practices.
     - The multifaceted world of programming beckons me with its promise of limitless possibilities. 
     - In my pursuit of knowledge, I intend to immerse myself in various programming languages such as Python, Java, and C++, unraveling their unique syntax and functionalities. 
     - Beyond the syntax, I am eager to comprehend the underlying principles that govern the creation of software solutions. Through hands-on projects and practical applications, I aim to hone my coding skills, fostering a mastery that extends beyond theoretical knowledge.
