6dce0738b91d62e3cc6f83acd6cbc2808b1f9677
janreaneelico <153175210+janreaneelico@users.noreply.github.com>
2023-12-07 17:13:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.write("Message me on Facebook [Click here >](https://www.facebook.com/profile
 st.write("---")
 st.header("About me:")
 st.write("##")
-st.subheader(
+st.write(
     """
     - Actually I love programming when I was in my senior high I always have this dreams/goals that I want to be a programmer someday.
     - I have a fervent desire to embark on a journey of continuous learning, particularly in the realm of programming and coding. The allure of these disciplines captivates me, and I am resolute in my commitment to unravel their complexities. 
@@ -53,7 +53,7 @@ st.subheader(
 st.write("---")
 st.header("More Info About me:")
 st.write("##")
-st.subheader(
+st.write(
     """
     - In my pursuit of knowledge, I intend to immerse myself in various programming languages such as Python, Java, and C++, unraveling their unique syntax and functionalities. 
     - Beyond the syntax, I am eager to comprehend the underlying principles that govern the creation of software solutions. Through hands-on projects and practical applications, I aim to hone my coding skills, fostering a mastery that extends beyond theoretical knowledge.
