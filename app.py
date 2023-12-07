83e00bd5b88ac8c0c42837754e1cbda34962654d
janreaneelico <153175210+janreaneelico@users.noreply.github.com>
2023-12-07 17:09:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,25 @@ st.write("Message me on Facebook [Click here >](https://www.facebook.com/profile
 st.write("---")
 st.header("About me:")
 st.write("##")
-st.write(
+st.subheader(
     """
     - Actually I love programming when I was in my senior high I always have this dreams/goals that I want to be a programmer someday.
     - I have a fervent desire to embark on a journey of continuous learning, particularly in the realm of programming and coding. The allure of these disciplines captivates me, and I am resolute in my commitment to unravel their complexities. 
     - As I set forth on this educational odyssey, my primary objective is to cultivate a profound understanding of programming languages and coding practices.
     - The multifaceted world of programming beckons me with its promise of limitless possibilities. 
+    - In my pursuit of knowledge, I intend to immerse myself in various programming languages such as Python, Java, and C++, unraveling their unique syntax and functionalities. 
+    - Beyond the syntax, I am eager to comprehend the underlying principles that govern the creation of software solutions. Through hands-on projects and practical applications, I aim to hone my coding skills, fostering a mastery that extends beyond theoretical knowledge.
+    - Furthermore, my curiosity extends to the broader landscape of computers. I am intrigued by the intricate interplay of hardware and software, and I am determined to explore the symbiotic relationship between the two. 
+    - From the fundamentals of computer architecture to the intricacies of operating systems, my quest for knowledge encompasses the holistic understanding of computers. In essence, my journey in programming and coding is not merely about acquiring skills but about cultivating a profound appreciation for the art and science of computational thinking. 
+    - With each line of code written and every algorithm deciphered, I am poised to unravel the vast tapestry that is the world of programming and computers.
+    """
+)
+
+st.write("---")
+st.header("More Info About me:")
+st.write("##")
+st.subheader(
+    """
     - In my pursuit of knowledge, I intend to immerse myself in various programming languages such as Python, Java, and C++, unraveling their unique syntax and functionalities. 
     - Beyond the syntax, I am eager to comprehend the underlying principles that govern the creation of software solutions. Through hands-on projects and practical applications, I aim to hone my coding skills, fostering a mastery that extends beyond theoretical knowledge.
     - Furthermore, my curiosity extends to the broader landscape of computers. I am intrigued by the intricate interplay of hardware and software, and I am determined to explore the symbiotic relationship between the two. 
